d1f575c0aaf21ff81c660f04b3f9f65f5548891d
roger3g <rogerluiz010@email.com>
2021-01-24 16:20:43 -0300

    added change real time

--- script.js
+++ script.js
@@ -1,48 +1,25 @@
-const Rrange = document.querySelector('#r-range')
-const Grange = document.querySelector('#g-range')
-const Brange = document.querySelector('#b-range')
+window.addEventListener('load', () => {
+  const Rrange = document.querySelector('#r-range')
+  const Grange = document.querySelector('#g-range')
+  const Brange = document.querySelector('#b-range')
 
-const Rinput = document.querySelector('#r-input')
-const Ginput = document.querySelector('#g-input')
-const Binput = document.querySelector('#b-input')
+  const outputColors = document.querySelector('#output-colors')
+  const copy = document.querySelector('#copy')
 
-const outputColors = document.querySelector('#output-colors')
-const copy = document.querySelector('#copy')
+  Rrange.addEventListener('change', ()=> {
+    chageColor(Rrange.value, Grange.value, Brange.value)
+  })
 
-const button = document.querySelector('button')
+  Grange.addEventListener('change', ()=> {
+    chageColor(Rrange.value, Grange.value, Brange.value)
+  })
 
-button.addEventListener('click', () => {
+  Brange.addEventListener('change', ()=> {
+    chageColor(Rrange.value, Grange.value, Brange.value)
+  })
 
-  if ( Rinput.value == '' || Ginput.value == '' || Binput.value == '' ) {
-    chageColorRange(Rrange.value, Grange.value, Brange.value)
-  } else {
-    Rrange.value = '128'
-    Grange.value = '128'
-    Brange.value = '128'
-    chageColorInput(Rinput.value, Ginput.value, Binput.value)
-    Rinput.value = ''
-    Ginput.value = ''
-    Binput.value = ''
+  const chageColor = (colorRed, colorGreen, colorBlue) => {
+    outputColors.style.backgroundColor = `rgb(${colorRed},${colorGreen},${colorBlue})`
+    copy.innerHTML = `rgb(${colorRed},${colorGreen},${colorBlue})`
   }
-
 })
-
-function chageColorRange(r, g, b) {
-  outputColors.style.backgroundColor = `rgb(${r},${g},${b})`
-  copy.innerHTML = `rgb(${r},${g},${b})`
-}
-
-function chageColorInput(r, g, b) {
-  if ( r > 255 || g > 255 || b > 255 ) {
-    r = '255'
-    g = '255'
-    b = '255'
-  } else if ( r < 0 || g < 0 || b < 0 ) {
-    r = '0'
-    g = '0'
-    b = '0'
-  }
-
-  outputColors.style.backgroundColor = `rgb(${r},${g},${b})`
-  copy.innerHTML = `rgb(${r},${g},${b})`
-}
